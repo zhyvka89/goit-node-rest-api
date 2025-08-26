e0dfacc61ad531ddd55904de9e17e213ce99eb9c
julia <julia.zhyvylo@isolutions.mk>
2025-08-26 09:35:30 +0200

    fix mentor coments

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -22,7 +22,7 @@ export const sendResponse = (res, statusCode, data) => {
 export const getAllContacts = async (req, res, next) => {
   try {
     const contacts = await listContacts();
-    sendResponse(res, 200, { contacts });
+    return sendResponse(res, 200, contacts );
   } catch (error) {
     next(error);
   }
@@ -33,9 +33,9 @@ export const getOneContact = async (req, res, next) => {
     const { id } = req.params;
     const contact = await getContactById(id);
     if (!contact) {
-      next(HttpError(404, "Not found"));
+      return next(HttpError(404, "Not found"));
     }
-    sendResponse(res, 200, { contact });
+    return sendResponse(res, 200, contact );
   } catch (error) {
     next(error);
   }
@@ -44,11 +44,11 @@ export const getOneContact = async (req, res, next) => {
 export const deleteContact = async (req, res, next) => {
   try {
     const { id } = req.params;
-    const removedContact = removeContact(id);
+    const removedContact = await removeContact(id);
     if (!removedContact) {
-      next(HttpError(404, "Not found"));
+      return next(HttpError(404, "Not found"));
     }
-    sendResponse(res, 200, { removedContact });
+    return sendResponse(res, 200, removedContact );
   } catch (error) {
     next(error);
   }
@@ -58,30 +58,31 @@ export const createContact = async (req, res, next) => {
   try {
     const { error } = createContactSchema.validate(req.body);
     if(error) {
-      next(HttpError(400, error.details[0].message));
+      return next(HttpError(400, error.details[0].message));
     }
+    const { name, email, phone } = req.body;
     const newContact = await addContact(name, email, phone);
-    sendResponse(res, 201,  newContact );
+    return sendResponse(res, 201,  newContact );
   } catch (error) {
     next(error);
   }
 };
 
-export const updateContact = async (req, res) => {
+export const updateContact = async (req, res, next) => {
   try {
     if (Object.keys(req.body).length === 0) {
-      next(HttpError(400, "Body must have at least one field"));
+      return next(HttpError(400, "Body must have at least one field"));
     }
     const { error } = updateContactSchema.validate(req.body);
     if (error) {
-      next(HttpError(400, error.details[0].message));
+      return next(HttpError(400, error.details[0].message));
     }
     const { id } = req.params;
     const updatedContact = await updateContactWithNewFields(id, req.body);
     if (!updatedContact) {
-      next(HttpError(404, "Not found"));
+      return next(HttpError(404, "Not found"));
     }
-    sendResponse(res, 200, { updatedContact });
+    return sendResponse(res, 200, updatedContact );
   } catch (error) {
     next(error);
   }
