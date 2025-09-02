ccb28628ff1f6f310f3c1abaff98086db3531a07
julia <julia.zhyvylo@isolutions.mk>
2025-09-02 19:51:16 +0200

    add token

--- services/authServices.js
+++ services/authServices.js
@@ -1,6 +1,9 @@
 import User from "../db/User";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import HttpError from "../helpers/HttpError";
+
+const { JWT_SECRET } = process.env;
 
 export const register = async (email, password) => {
   const hashPassword = await bcrypt.hash(password, 10);
@@ -16,5 +19,10 @@ export const login = async (email, password) => {
   if (!passwordCompare) {
     throw HttpError(401, "Email or password is wrong");
   }
+
+  const payload = { id: user.id };
+  const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "24h" });
+  user.token = token;
+  await user.save();
   return user;
 };
